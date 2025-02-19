965cc586328be4ae0caecaf60d09f10f17acf5c1
v0l0 <podolsky35alex@gmail.com>
2025-02-19 23:26:48 +0300

    kursa4

--- assets/menu-shadow.js
+++ assets/menu-shadow.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function toggleMenuBackground() {
         const modal = document.querySelector(".modal")?.style.display === "flex";
     {
-            if (window.scrollY > 100 ) {
+            if (window.scrollY > 50 ) {
                 header.classList.add("menu-active");
             }else if(!modal){
                 header.classList.remove("menu-active");
